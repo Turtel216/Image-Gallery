92d73d1241d446df60163fb218beb8e89130024e
Turtel216 <papadimitrios216@gmail.com>
2024-06-03 09:09:15 +0300

    Created NextJS Project

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'Image-Gallery',
+  description: 'A Poject by Dimitrios Papakonstantinou',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <main className="max-w-6xl mx-auto">
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
