6e8a64326c37dc37ca61e0e4235d416852503401
Turtel216 <papadimitrios216@gmail.com>
2024-06-03 14:13:54 +0300

    Added Image container

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
+const { hostname } = require('os')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/photos/**'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
